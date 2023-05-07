b6b594685e9ca5b390d96243b5adcbfc210fe762
ehaabd <ehaab14@hotmail.com>
2023-05-07 13:54:15 -0700

    Improvement to logic, nearly complete
    Updated code to change box colors when scored. Improved restart button function to account for colored boxes.
    
    Logic gate needs to be improved for winning tiles, and final scoring algorithm needs to be created.

--- logic.js
+++ logic.js
@@ -3,6 +3,8 @@ let restartBtn = document.getElementById('restartBtn');
 let boxes = Array.from(document.getElementsByClassName('box'));
 let boxesMini = Array.from(document.getElementsByClassName('boxMini'));
 
+//X corresponds to blue
+//O corresponds to yellow
 const O_TEXT = "O";
 const X_TEXT = "X";
 let currentPlayer = X_TEXT;
@@ -24,34 +26,46 @@ const winningCombos = [
 
 const startGame = () => {
     boxesMini.forEach(boxMini => boxMini.addEventListener('click', boxMiniClicked));
+    restartBtn.addEventListener('click', restart)
 }
 
-//Next playable box = position % 9
-//Calculate board array and winning positions based on (e - (e % 9))/9
-//Can only select on tiles in playable boxes
+//Error with logic gate determining if player has won - needs to be recreated in multiple parts, including the involvement of Y
 
 function boxMiniClicked(e){
     const id = e.target.id;
-    console.log(id);
 
-    if(!spacesMini[id]){
-        spacesMini[id] = currentPlayer;
-        e.target.innerText = currentPlayer;
+    if(id>(9*playableBox-1) && id<(9*playableBox+9)){
+        if(!spacesMini[id]){
+            spacesMini[id] = currentPlayer;
+            e.target.innerText = currentPlayer;
+    
+            if(playerHasWonMini()){
+                if(spaces[playableBox]==X_TEXT||spaces[playableBox]==O_TEXT){
+                    console.log('you should never run');
+                    spaces[playableBox] = 'Y';
+                    boxes[playableBox].style.backgroundColor='#00ff00';
+                }else if(currentPlayer == X_TEXT){
+                    console.log('why are you running');
+                    spaces[playableBox] = X_TEXT;
+                    boxes[playableBox].style.backgroundColor = '#1582ca';
+                }else{
+                    console.log('you should run');
+                    spaces[playableBox] = O_TEXT;
+                    boxes[playableBox].style.backgroundColor = '#8f8a29';
+                }      
+            }
 
-        if(playerHasWonMini()){
-            console.log('good job. u beat the game ig.');
-            
+            playableBox = (id%9);
+            currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
         }
-
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
-    }
+    } 
 }
 
-function playerHasWonMini() {
-    for (const condition of winningCombos) {
-        let [a, b, c] = condition;
+function playerHasWonMini(){
+    for(const condition of winningCombos){
+        let a = condition[0], b = condition[1], c = condition[2];
 
-        if(spacesMini[a] && (spacesMini[a] == spacesMini[b] && spacesMini[a] == spacesMini[c])) {
+        if(((spacesMini[a+(playableBox*9)] !== null) && (spacesMini[a+(playableBox*9)] == spacesMini[b+(playableBox*9)] && spacesMini[a+(playableBox*9)] == spacesMini[c+(playableBox*9)]))){
             return true;
         }
     }
@@ -69,26 +83,26 @@ function playerHasWon() {
     return false;
 }
 
-
-restartBtn.addEventListener('click', restart)
-
 function restart() {
     spaces.fill(null);
+    spacesMini.fill(null);
 
     boxes.forEach( box => {
-        box.innerText = ''
         box.style.backgroundColor = '';
     })
-    boxesMini.forEach( box => {
-        boxMini.innerText = '';
-        boxMini.style.backgroundColor = '';
 
+    boxesMini.forEach( boxMini=> {
+        boxMini.innerText = '';
     })
+
+    playableBox = 4;
 
     playerText.innerHTML = 'Tic Tac Toe';
 
     currentPlayer = X_TEXT;
 }
+
+startGame()
 
 
 
@@ -170,10 +184,3 @@ function boxClicked(e) {
         
     }
 }
-
-
-
-
-
-
-startGame()
